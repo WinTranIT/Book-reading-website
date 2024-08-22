4127b9047d3025b5e8a09aabd424312657626a8f
thuynehe <nguyenthithuythuy123@gnmail.com>
2024-08-22 17:51:28 +0700

    fix: UI Home page

--- BookAPI/BookAPI/obj/Debug/net8.0/BookAPI.AssemblyInfo.cs
+++ BookAPI/BookAPI/obj/Debug/net8.0/BookAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e899262394b7bf354ddada09d2ca6f7acd85d154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9826dd7d4e8c8c2d0a39f3db58960069fdc32543")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
